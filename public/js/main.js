ba83f9641c192bd1a72f943121a4563d0d6de9a8
TFX0019 <1400kss@gmail.com>
2019-07-31 16:38:59 -0430

    correccione1

--- public/js/main.js
+++ public/js/main.js
@@ -22,17 +22,6 @@ $(document).ready(function() {
 
     }
 
-    $(function() {
-        var $visor = document.getElementById('visor');
-        setTimeout(() => {
-            var visorContainer = document.querySelector('.imgContainer');
-            visorContainer.style.height = $visor.clientHeight + 'px';
-        }, 100);
-        console.log($visor.clientHeight)
-    })
-
-
-
     $('.showCategorias').on('click', function () {
         glob.showCategorias();
     })
@@ -40,61 +29,132 @@ $(document).ready(function() {
         glob.showCategorias();
     })
 
-    // revista
-    $(function () {
-        $('.revista').owlCarousel({
-            items: 1,
-            margin: 0,
-            loop: false,
-            URLhashListener: false,
-            onInitialized: totalItem
-        })
-        function totalItem(e) {
-            glob.totalPages = '0' + (e.item.count);
-            $('#pagination-premios').text("0" + (e.item.index + 1) + " / " + glob.totalPages)
-        }
-        $('.revista').on('changed.owl.carousel', function (e) {
-            $('#pagination-premios').text("0" + (e.item.index + 1) + " / " + glob.totalPages)
-        })
-    })
-    // collages
-            $(function () {
-                $('.collage').owlCarousel({
-                    center: true,
-                    items: 3,
-                    margin: -10,
-                    loop: true,
-                    autoplay: true
-                })
+if ($('.revista').length !== 0 || $('.collage').length !== 0) {
+        // revista
+        $(function () {
+            $('.revista').owlCarousel({
+                items: 1,
+                margin: 0,
+                loop: false,
+                URLhashListener: false,
+                onInitialized: totalItem
             })
+
+            function totalItem(e) {
+                glob.totalPages = '0' + (e.item.count);
+                $('#pagination-premios').text("0" + (e.item.index + 1) + " / " + glob.totalPages)
+            }
+            $('.revista').on('changed.owl.carousel', function (e) {
+                $('#pagination-premios').text("0" + (e.item.index + 1) + " / " + glob.totalPages)
+            })
+        })
+        // collages
+        $(function () {
+            $('.collage').owlCarousel({
+                center: true,
+                items: 3,
+                margin: -10,
+                loop: true,
+                autoplay: true
+            })
+        })
+}
 //texto de contactos
-$(function() {
-    $('.contactTitle').arctext({
-        radius: 500
-    });
-})
+if ($('.contactTitle').length !== 0) {
+ $(function () {
+     $('.contactTitle').arctext({
+         radius: 500
+     });
+ })
+}
+
 //abrir menu principal
-    glob.btnMenuShow.addEventListener('click', function() {
-       glob.menuContainer[0].style.left = 0
-    })
+            glob.btnMenuShow.addEventListener('click', function () {
+                glob.menuContainer[0].style.left = 0
+            })
 
-$(function () {
-    glob.btnMenuClose.addEventListener('click', function () {
-        glob.menuContainer[0].style.left = '-100%'
-    })
+            glob.btnMenuClose.addEventListener('click', function () {
+                glob.menuContainer[0].style.left = '-100%'
+            })
+
+if ($(".home").length !== 0 || $(".coleccion-p").length !== 0) {
+        // scroll home page
+        $(".home").onepage_scroll({
+            sectionContainer: "section",
+            easing: "ease",
+            animationTime: 1000,
+            pagination: true,
+            updateURL: false,
+            loop: false,
+            keyboard: false,
+            responsiveFallback: false,
+            direction: "vertical"
+        });
+
+        // scroll de colecciones
+
+        $('.coleccion-p').onepage_scroll({
+            sectionContainer: "section",
+            easing: "ease",
+            animationTime: 1000,
+            pagination: true,
+            updateURL: false,
+            loop: false,
+            keyboard: false,
+            responsiveFallback: false,
+            direction: "vertical"
+        });
+}
+   
+// paginas de revista
+$('#togglePagesRevista').on('click', function(e) {
+    e.preventDefault()
+    $('.modalPaginationPremios').slideToggle();
+})
+// cerrar premios
+$('.closePagination button').on('click', function() {
+    $('.modalPaginationPremios').slideToggle();
+})
+//search
+$('.search').hide();
+$('#openSearch').on('click', function() {
+    $('.search').fadeIn('slow');
+})
+$('.closeSearch').on('click', function () {
+    $('.search').fadeOut('slow');
+})
+// help
+$('.modalhelpsearch').hide();
+$('.openhelpremios').on('click', function(e) {
+    e.preventDefault();
+    $('.modalhelpsearch').fadeIn('slow');
+})
+$('.closeModalhelp').on('click', function () {
+   $('.modalhelpsearch').fadeOut('slow');
+})
+// contacto
+$('.openContacto').on('click', function() {
+    $('.contacto').css('opacity', 1);
+    $('.contacto').css('bottom', 0);
+    $('.info-contacto').css('bottom', 0);
 })
 
-// scroll 
-    $(".home").onepage_scroll({
-    sectionContainer: "section",
-    easing: "ease",
-    animationTime: 1000,
-    pagination: true,
-    updateURL: false,
-    loop: false,
-    keyboard: false,
-    responsiveFallback: false,
-    direction: "vertical"
-});
+$('.closeContact').on('click', function () {
+    $('.contacto').css('opacity', 0);
+    $('.contacto').css('bottom', '-100%');
+    $('.info-contacto').css('bottom', '-100%');
+})
+
+
+
+
+        $(function () {
+            var $visor = document.getElementById('visor');
+            setTimeout(() => {
+                var visorContainer = document.querySelector('.imgContainer');
+                visorContainer.style.height = $visor.clientHeight + 'px';
+            }, 100);
+            console.log($visor.clientHeight)
+        })
 
 })
